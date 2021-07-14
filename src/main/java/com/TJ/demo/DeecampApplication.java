57f069be347f43178913e31311262c76d97ceb84
ruaruaruabick <993492964@qq.com>
2021-07-14 11:12:34 +0800

    Initial commit

---
+++ src/main/java/com/TJ/demo/DeecampApplication.java
@@ -0,0 +1,13 @@
+package com.TJ.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeecampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeecampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TJ/demo/DeecampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TJ.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeecampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
